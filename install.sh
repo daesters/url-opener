ac1905db5350950a9a82af0773261f4c300ab2dd
Simon <simon.daester@gmail.com>
2020-02-21 11:38:22 +0100

    Inserted uninstalling functions

--- install.sh
+++ install.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
+# PATHS
+
 SCRIPTPATH=`pwd`
 SCRIPT="URL-opener"
-
-LINK="/usr/local/bin/${SCRIPT,,} "
 
 LOCALPATH="$HOME/.local/bin"
 GLOBALPATH="/usr/local/bin"
 CMD=${SCRIPT,,}
+
+
+ICON="web.jpg"
+ICONDEST="$HOME/.local/share/icons/hicolor/128x128/apps/$SCRIPT.jpg"
+
+DESKTOPFILE="URL-opener.desktop"
+DESKTOPPATH="$HOME/.local/share/applications/"
 
 ### Functions
 
@@ -25,30 +32,58 @@ function copy_script() {
 	cp $SCRIPTPATH/$SCRIPT $1/$CMD
 }
 
-function copy_desktop() {	
-	echo "Copying desktop files..."
-	cp web.jpg "$HOME/.local/share/icons/hicolor/128x128/apps/URL-opener.jpg"
-	cp "URL-opener.desktop" "$HOME/.local/share/applications/"
+function copy_script_sudo() {
+	sudo cp $SCRIPTPATH/$SCRIPT $1/$CMD
 }
 
-### Main
+function copy_desktop() {	
+	echo "Copying desktop files..."
+	cp $ICON $ICONDEST
+	cp $DESKTOPFILE $DESKTOPPATH
+}
 
-check_local_path
-res=$?
-if [ $res -gt 0 ]
+
+### Installation and uninstallation functions
+
+function installation() {
+	check_local_path
+	res=$?
+	if [ $res -gt 0 ]
+	then
+		echo "Install in local path"
+		copy_script $LOCALPATH	
+	else
+		read -p "Install in global path, needs sudo rights (y/n)?" choice
+		case "$choice" in 
+		  y|Y ) copy_script_sudo $GLOBALPATH;;
+		  * ) echo "abort" && exit;;
+		esac
+	fi
+
+	copy_desktop
+
+	echo "Done"
+}
+
+function uninstallation() {
+	rm -f $DESKTOPPATH$DESKTOPFILE
+	rm -f $ICONDEST
+	rm -f $LOCALPATH/$CMD
+
+	if [ -f $GLOBALPATH/$CMD ]
+	then
+		sudo rm $GLOBALPATH/$CMD
+	fi	
+}
+
+### MAIN
+if [[ $1 =~ "-u" ]]
 then
-	echo "Install in local path"
-	copy_script $LOCALPATH	
+	echo "Uninstall"
+	uninstallation
 else
-	read -p "Install in global path, needs sudo rights (y/n)?" choice
-	case "$choice" in 
-	  y|Y ) sudo copy_script $PATH;;
-	  * ) echo "abort" && exit;;
-	esac
+	echo "Installation"
+	installation
 fi
-
-copy_desktop
-
-echo "Done"
 
 
